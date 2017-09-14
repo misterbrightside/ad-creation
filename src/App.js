0d2273e94047ef5f8ebda5f22ddcf88fc4217820
johnbrennandaft <john.brennan@distilled.ie>
2017-09-14 23:01:15 +0100

    fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { injectGlobal } from 'styled-components';
 import AdCreation from './components/AdCreation';
-
-injectGlobal`
-  body {
-    margin: 0;
-    padding: 0;
-  }
-`;
 
 const App = () => (
   <BrowserRouter>
--- src/components/AdCreation/Location/index.js
+++ src/components/AdCreation/Location/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import Input from '../../Input';
 
-const AutoAddress = () => (<div></div>);
+const AutoAddress = () => (
+  <div>
+    <input />
+  </div>
+);
 
 const Wrapper = styled.div`
-  background: green;
   display: grid;
-  grid-gap: 1rem;
-  padding: 1rem 0;
-  justify-content: center;
+  justify-items: center;
+`;
+
+const Header = styled.h1`
+  text-align: center;
 `;
 
 const Location = () => (
   <Wrapper>
-    <h1>What is the address of the place you're selling?</h1>
+    <Header>What's the address of the place you're selling?</Header>
     <AutoAddress />
   </Wrapper>
 );
